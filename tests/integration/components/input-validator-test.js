14d9ab5e30aa704551f99c3374ee92ee7c1e7c75
Bill Dami <bdami@gavant.com>
2016-10-12 16:16:29 -0400

    allow the label for="" attribute to be set for select and textarea elements, make sure only the input-validators label element is modified

--- tests/integration/components/input-validator-test.js
+++ tests/integration/components/input-validator-test.js
@@ -31,3 +31,52 @@ test('Show error', function(assert) {
 
   assert.equal(this.$('.input-validation-error b').text().trim(), 'Name can not be empty');
 });
+
+test('Updates label\'s for="" attribute - input', function(assert) {
+  // Template block usage:
+  this.render(hbs`
+    {{#input-validator target="model.name" text="Name"}}
+        {{input type="text" value=test}}
+    {{/input-validator}}
+  `);
+
+  assert.equal(this.$('label.input-validator-label').attr('for'), this.$('input:first').attr('id'));
+});
+
+test('Updates label\'s for="" attribute - select', function(assert) {
+  // Template block usage:
+  this.render(hbs`
+    {{#input-validator target="model.name" text="Name"}}
+        <select id="select-id">
+            <option value="test">Test</option>
+        </select>
+    {{/input-validator}}
+  `);
+
+  assert.equal(this.$('label.input-validator-label').attr('for'), this.$('select:first').attr('id'));
+});
+
+test('Updates label\'s for="" attribute - textarea', function(assert) {
+  // Template block usage:
+  this.render(hbs`
+    {{#input-validator target="model.name" text="Name"}}
+        {{textarea value=test}}
+    {{/input-validator}}
+  `);
+
+  assert.equal(this.$('label.input-validator-label').attr('for'), this.$('textarea:first').attr('id'));
+});
+
+test('Other label elements\' for="" attributes are not modified', function(assert) {
+  // Template block usage:
+  this.render(hbs`
+    <label class="external-label" for="unchanged">External Label</label>
+    {{#input-validator target="model.name" text="Name"}}
+        {{input type="text" value=test}}
+        <label class="other-internal-label" for="unchanged">Other internal label</label>
+    {{/input-validator}}
+  `);
+
+  assert.equal(this.$('label.external-label').attr('for'), 'unchanged');
+  assert.equal(this.$('label.other-internal-label').attr('for'), 'unchanged');
+});
